fed7dd2dad819a63df1383c418b39189b0670aab
Matt Varghese <16229800+mattvarghese@users.noreply.github.com>
2025-12-27 22:30:53 -0600

    copyright comments

--- compiler/codegen.cpp
+++ compiler/codegen.cpp
@@ -1,3 +1,26 @@
+/* Copyright 2005-2015 Varghese Mathew (Matt)
+ *
+ * This file is part of Coconut (TM).
+ * Coconut is a
+ *     Multi-threaded simulation of the pipeline of a MIPS-like
+ *     Microprocessor (integer instructions only) replete with 
+ *     Memory Subsystem, Caches and their performance analysis,
+ *     I/O device modules and an assembler.
+ * 
+ * Coconut is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Coconut is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Coconut.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "codegen.h"
 #include "util.h"
 #include <sstream>
